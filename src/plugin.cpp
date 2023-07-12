ddc7aa519d26fa00f06f38d306ec5b63a9cedf6f
engsr6982 <pwxg286704@163.com>
2023-07-12 22:11:27 +0800

    没问题，但操蛋的编译不过

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,14 +1,47 @@
+#if 0
+TMP=$(mktemp -d)
+c++ -std=c++11 -I src -o ${TMP}/a.out ${0} && ${TMP}/a.out ${@:1} ; RV=${?}
+rm -rf ${TMP}
+exit ${RV}
+#endif
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "rapidcsv.h"
+
+#include "version.h"
 #include <llapi/LoggerAPI.h>
 #include <llapi/EventAPI.h>
 #include <llapi/ServerAPI.h>
-
-#include <iostream>
-#include "version.h"
+#include <llapi/RemoteCallAPI.h>
 
 extern Logger logger;
 
+std::vector<std::vector<std::string>> csvTo2DArray(const std::string &csvFilePath)
+{
+    rapidcsv::Document doc(csvFilePath);
+    std::vector<std::vector<std::string>> data;
+
+    for (size_t i = 0; i < doc.GetRowCount(); ++i)
+    {
+        data.push_back(doc.GetRow<std::string>(i));
+    }
+
+    return data;
+}
+
 void exportToJavaScriptAPI()
 {
+    RemoteCall::exportAs("_CSV_JSON_", "_csvToJson_", [&](std::string name) -> std::vector<std::vector<std::string>>
+                         {
+                         logger.info("开始转换文件: {}", name);
+                         auto start = std::chrono::high_resolution_clock::now();
+                         auto s = csvTo2DArray(name);
+                         auto end = std::chrono::high_resolution_clock::now();
+                         auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+                         logger.info("转换文件[{}]完成，耗时: {}ms", name, duration);
+                         return s; });
 }
 
 void PluginInit()
